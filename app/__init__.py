dfdcf2f5d53bd2e2a220adf8ca41737d01fba735
Simón Villafañe <simonvillafanee@gmail.com>
2025-08-03 22:50:34 -0300

    docs(Root): swagger openapi 3.1.x

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_cors import CORS
 from flask_socketio import SocketIO
+from flask_swagger_ui import get_swaggerui_blueprint # type: ignore
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "Shh!"
@@ -8,8 +9,8 @@ app.config["SECRET_KEY"] = "Shh!"
 CORS(app, resources={r"/*": {"origins": "*"}})
 socketio = SocketIO(
     app,
-    ping_interval=10,
-    ping_timeout=20,  # heartbeat
+    ping_interval=10, # heartbeat
+    ping_timeout=20,
     logger=True,
     engineio_logger=True,
     cors_allowed_origins=[
@@ -23,6 +24,36 @@ socketio = SocketIO(
 from app.events import *
 from app.blueprints import v1_bp
 
+
+SWAGGER_URL = '/api/docs'  # URL for exposing Swagger UI (without trailing '/')
+API_URL = 'http://127.0.0.1:8000/spec'  # URL for exposing API documentation (without trailing '/')
+
+# Call factory function to create our blueprint
+swaggerui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,  # Swagger UI static files will be mapped to '{SWAGGER_URL}/dist/'
+    API_URL,
+    config={  # Swagger UI config overrides
+        'app_name': "Test application"
+    },
+    # oauth_config={  # OAuth config. See https://github.com/swagger-api/swagger-ui#oauth2-configuration .
+    #    'clientId': "your-client-id",
+    #    'clientSecret': "your-client-secret-if-required",
+    #    'realm': "your-realms",
+    #    'appName': "your-app-name",
+    #    'scopeSeparator': " ",
+    #    'additionalQueryStringParams': {'test': "hello"}
+    # }
+)
+
+@app.route("/spec")
+def spec():
+    from pathlib import Path
+    from os import getcwd
+    from json import loads
+    raw_json = Path(f"{getcwd()}/docs/swagger.json").read_text()
+    swag = loads(raw_json)
+    return jsonify(swag)
+
 @app.route("/ping")  
 def ping():
     from time import time
@@ -31,4 +62,5 @@ def ping():
     end = time() * 1000
     return f"Pong in {end - start:.2f}ms"
 
+app.register_blueprint(swaggerui_blueprint)
 app.register_blueprint(v1_bp)
