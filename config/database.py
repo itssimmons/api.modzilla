0618027e2c7239f3d2ee796b8fbde4d315148bd4
Simón Villafañe <simonvillafanee@gmail.com>
2025-08-07 22:48:51 -0300

    core(Root): flag dml to not add `<table>.` prefix before fields if the query is not a `SELECT`

--- config/database.py
+++ config/database.py
@@ -1,6 +1,6 @@
 import sqlite3
 from os import getcwd
-from typing import List, Tuple, Any, Dict, Literal, overload
+from typing import List, Tuple, Any, Dict, Literal, overload, Callable
 
 DATABASE_PATH = f"{getcwd()}/database/db.sqlite3"
 SQL_TEMPLATE = ["", "", "", "", "", ""]
@@ -10,6 +10,7 @@ class Builder:
     __f: Literal["*"] | List[str] = "*"
     __v: Tuple[Any, ...] = ()
     __rel: List[str] = []
+    __dml: Literal["INSERT", "SELECT", "UPDATE", "DELETE"] = "INSERT"
 
     __con: sqlite3.Connection | None = None
     __cur: sqlite3.Cursor | None = None
@@ -26,6 +27,7 @@ class Builder:
 
     @staticmethod
     def raw_query(sql: str):
+        """The passed sql query is executed inmmediately and the result is returned as raw data"""
         con = Builder.connect()
         cur = con.execute(sql)
         con.commit()
@@ -44,39 +46,44 @@ class Builder:
     @overload
     def parse_fields( # type: ignore
         fields: Literal["*"] | List[str],
-        t: str | None = None,
+        table: str,
         cast: Literal["str"] = "str",
         prefix: bool = False,
+        dml: Literal["INSERT", "SELECT", "UPDATE", "DELETE"] = "INSERT",
     ) -> str: ...
     @staticmethod
     @overload
     def parse_fields( # type: ignore
         fields: Literal["*"] | List[str],
-        t: str | None = None,
+        table: str,
         cast: Literal["list"] = "list",
         prefix: bool = False,
+        dml: Literal["INSERT", "SELECT", "UPDATE", "DELETE"] = "INSERT",
     ) -> List[str]: ...
     @staticmethod
     def parse_fields(
         fields: Literal["*"] | List[str],
-        t: str | None = None,
+        table: str,
         cast: Literal["list", "str"] = "str", # type: ignore
         prefix: bool = False,
+        dml: Literal["INSERT", "SELECT", "UPDATE", "DELETE"] = "INSERT",
     ) -> List[str] | str:
-        f: List[str] | Literal["*"] = fields
+        mutable_fields: List[str] | Literal["*"] = fields
 
-        if f == "*" and t is not None:
-            f = Builder.resolve_asterisk(t)
+        fn: Callable[[str], str] = lambda x: f"{table}.{x}" if dml == "SELECT" else x
 
-        if f != "*" and cast == "list":
+        if mutable_fields == "*":
+            mutable_fields = Builder.resolve_asterisk(table)
+        
+        if cast == "list":
             if prefix is not False:
-                return [f"{t}.{f} AS '{t}:{f}'" for f in f]
-            return [f"{t}.{f}" for f in f]
+                return [f"{fn(f)} AS '{table}:{f}'" for f in mutable_fields]
+            return [f"{fn(f)}" for f in mutable_fields]
         
         if prefix is not False:
-            return ", ".join([f"{t}.{f} AS '{t}:{f}'" for f in f])
+            return ", ".join([f"{fn(f)} AS '{table}:{f}'" for f in mutable_fields])
 
-        return ", ".join([f"{t}.{f}" for f in f])
+        return ", ".join([f"{fn(f)}" for f in mutable_fields])
 
     @staticmethod
     def parse_values(values: Tuple[Any, ...]) -> str:
@@ -92,6 +99,8 @@ class Builder:
     @staticmethod
     def parse_rows(fields: List[str], rows: List[Any]) -> List[Dict[str, Any]]:
         result: List[Dict[str, Any]] = []
+        print(fields)
+        print(rows)
 
         for row in rows:
             json: Dict[str , Any] = dict()
@@ -111,6 +120,8 @@ class Builder:
     @staticmethod
     def parse_row(fields: List[str], row: Any) -> Dict[str, Any]:
         result: Dict[str, Any] = {}
+        print(fields)
+        print(row)
 
         for i in range(len(fields)):
             if ':' in fields[i]:
@@ -166,6 +177,7 @@ class Builder:
         self.__sql = list(SQL_TEMPLATE)
         self.__debug = debug
         self.__rows: List[Dict[str, Any]] = []
+        self.__rel: List[str] = []
 
         # public attributes
         self.count: int = 0
@@ -184,7 +196,7 @@ class Builder:
         if self.__cur is None:
             raise TypeError("Unexpected error: the cursor is None")
 
-        f: List[str] = self.__f # type: ignore
+        f: List[str] = Builder.parse_fields(self.__f, self.__t, cast="list", dml=self.__dml)
         entries = Builder.parse_rows(f, self.__rows)
 
         self.close()
@@ -196,7 +208,7 @@ class Builder:
         if self.__cur is None:
             raise TypeError("Unexpected error: the cursor is None")
 
-        f = Builder.parse_fields(self.__f, self.__t, cast="list")
+        f = Builder.parse_fields(self.__f, self.__t, cast="list", dml=self.__dml)
         entry = Builder.parse_row(f, self.__rows[0])
 
         self.close()
@@ -213,8 +225,9 @@ class Builder:
     def create(self):
         """Insert a new row into the table and returns the `lastrowid`
         of the inserted row or `-1` if the row isn't created"""
+        self.__dml = "INSERT"
 
-        f = Builder.parse_fields(self.__f, self.__t)
+        f = Builder.parse_fields(self.__f, self.__t, dml=self.__dml)
         v = self.__v
         placeholders = Builder.resolve_question_mark(len(v))
         self.__sql[0] = f"INSERT INTO {self.__t} ( {f} ) VALUES ( {placeholders} )"
@@ -229,20 +242,22 @@ class Builder:
 
     def read(self):
         """Read all rows from the table"""
+        self.__dml = "SELECT"
         
-        f = [Builder.parse_fields(self.__f, self.__t)]
+        f = [Builder.parse_fields(self.__f, self.__t, dml=self.__dml)]
 
         if len(self.__rel) > 0:
             for rel in self.__rel:
                 f.append(Builder.parse_fields('*', rel, prefix=True))
-        
-        f = ', '.join(f).split(', ')
-        self.__f = [x.split(' AS ')[0].replace('.', ':') if ' AS ' in x else x for x in f]
+            f = ', '.join(f).split(', ')
+            self.__f = [x.split(' AS ')[0].replace('.', ':') if ' AS ' in x else x for x in f]
+
         self.__sql[0] = f"SELECT {', '.join(f)} FROM {self.__t}"
         return self.__run()
 
     def update(self):
         """Update a row in the table"""
+        self.__dml = "UPDATE"
 
         if self.__f == "*":
             raise AssertionError("You can't update a row without specifying all fields")
@@ -254,9 +269,9 @@ class Builder:
         self.close()
 
     def delete(self):
-        """Delete a row from the table"""
+        """Delete a row from the table - Unstable ⚠️"""
+        self.__dml = "DELETE"
 
-        """Unstable ⚠️"""
         self.__sql[0] = f"DELETE FROM {self.__t}"
         self.__run()
         self.close()
