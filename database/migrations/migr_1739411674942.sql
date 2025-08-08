2df3f846ed428b17b697301eb771ee1c219a822c
Simón Villafañe <simonvillafanee@gmail.com>
2025-08-07 22:00:13 -0300

    fix(Root): missing sid and modified_by columns of prev versions

--- database/migrations/migr_1739411674942.sql
+++ database/migrations/migr_1739411674942.sql
@@ -1,9 +1,13 @@
 create table if not exists users (
     id integer primary key autoincrement,
+    sid text null,
+    modified_by integer null,
     username text not null,
     avatar text not null,
     color text not null,
     status text not null default 'offline',
     role text not null default 'user',
-    created_at text not null default current_timestamp
+    created_at text not null default current_timestamp,
+    
+    foreign key(modified_by) references users(id)
 )
